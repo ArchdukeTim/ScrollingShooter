024286d2437d56ba0e42d0ddccc1ab91404af57d
Tim Winters <twinters007@gmail.com>
2016-05-24 00:33:10 -0400

    Made shooting game

---
+++ conf.lua
@@ -0,0 +1,13 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Tim
+-- Date: 5/23/2016
+-- Time: 22:28
+-- To change this template use File | Settings | File Templates.
+--
+function love.conf(t)
+    t.title = "Scrolling Shooter"
+    t.window.width = 480
+    t.window.height = 800
+    t.console = true
+end
---
+++ main.lua
@@ -0,0 +1,158 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Tim
+-- Date: 5/23/2016
+-- Time: 22:37
+-- To change this template use File | Settings | File Templates.
+--
+
+debug = false
+
+player = {x = 200, y = 710, speed = 150, img = nil}
+
+canShoot = true
+canShootTimerMax = 0.2
+canShootTimer = canShootTimerMax
+
+bulletImg = nil
+
+bullets = {}
+
+
+createEnemyTimerMax = .4
+createEnemyTimer = createEnemyTimerMax
+
+enemyImg = nil
+
+enemies = {}
+
+isAlive = true
+score = 0
+
+function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
+end
+
+function love.load(arg)
+    player.img = love.graphics.newImage('assets/plane.png')
+    bulletImg =  love.graphics.newImage('assets/bullet.png')
+    enemyImg =   love.graphics.newImage('assets/enemy.png')
+end
+
+function love.update(dt)
+    if love.keyboard.isDown('escape') then
+        love.event.push('quit')
+    end
+
+    if love.keyboard.isDown('left', 'a') then
+        if player.x > 0 then
+          player.x = player.x - (player.speed*dt)
+        end
+
+    elseif love.keyboard.isDown('right', 'd') then
+        if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
+          player.x = player.x + (player.speed*dt)
+        end
+    end
+
+
+    canShootTimer = canShootTimer - (1 * dt)
+    if canShootTimer < 0 then
+      canShoot = true
+    end
+
+
+    if love.keyboard.isDown('space', 'rctrl', 'lctrl', 'ctrl') and canShoot then
+	       -- Create some bullets
+        newBullet = { x = player.x + (player.img:getWidth()/2), y = player.y, img = bulletImg }
+      	table.insert(bullets, newBullet)
+      	canShoot = false
+      	canShootTimer = canShootTimerMax
+    end
+
+    for i, bullet in ipairs(bullets) do
+    	bullet.y = bullet.y - (250 * dt)
+
+      	if bullet.y < 0 then -- remove bullets when they pass off the screen
+    		table.remove(bullets, i)
+    	end
+    end
+
+    createEnemyTimer = createEnemyTimer - (1 * dt)
+    if createEnemyTimer < 0 then
+    	createEnemyTimer = createEnemyTimerMax
+
+    	-- Create an enemy
+    	randomNumber = math.random(10, love.graphics.getWidth() - 10)
+    	newEnemy = { x = randomNumber, y = -10, img = enemyImg }
+    	table.insert(enemies, newEnemy)
+    end
+
+    -- update the positions of enemies
+    for i, enemy in ipairs(enemies) do
+    	enemy.y = enemy.y + (200 * dt)
+
+
+    	if enemy.y > 850 then -- remove enemies when they pass off the screen
+    		table.remove(enemies, i)
+    	end
+    end
+
+
+    for i, enemy in ipairs(enemies) do
+	for j, bullet in ipairs(bullets) do
+		if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), bullet.x, bullet.y, bullet.img:getWidth(), bullet.img:getHeight()) then
+			table.remove(bullets, j)
+			table.remove(enemies, i)
+			score = score + 1
+		end
+	end
+
+	if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), player.x, player.y, player.img:getWidth(), player.img:getHeight())
+	and isAlive then
+		table.remove(enemies, i)
+		isAlive = false
+    end
+
+
+
+
+
+    if not isAlive and love.keyboard.isDown('r') then
+        -- remove all our bullets and enemies from screen
+        bullets = {}
+        enemies = {}
+
+        -- reset timers
+        canShootTimer = canShootTimerMax
+        createEnemyTimer = createEnemyTimerMax
+
+        -- move player back to default position
+        player.x = 50
+        player.y = 710
+
+        -- reset our game state
+        score = 0
+        isAlive = true
+    end
+end
+end
+
+function love.draw(dt)
+    if isAlive then
+        love.graphics.draw(player.img, player.x, player.y)
+    else
+        love.graphics.print("Press 'R' to restart", love.graphics:getWidth()/2 - 50, love.graphics:getWidth()/2 - 10)
+    end
+
+    for i, bullet in ipairs(bullets) do
+        love.graphics.draw(bullet.img, bullet.x, bullet.y)
+    end
+
+    for i, enemy in ipairs(enemies) do
+        love.graphics.draw(enemy.img, enemy.x, enemy.y)
+    end
+end
